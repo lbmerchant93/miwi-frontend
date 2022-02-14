f4c1a085fa793cb08cce85dbc9a183fed9b74a36
Lucas Merchant <lbmerchant93@gmail.com>
2022-02-14 12:47:03 -0700

    Create initial AppBar for navigation

--- src/features/AppBar/AppBar.tsx
+++ src/features/AppBar/AppBar.tsx
@@ -1,11 +1,25 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { PossibleRoutes } from '../../utils/constants';
+import './AppBar.css'
 
 const AppBar: React.FC = () => {
-  return (
-    <nav>
-        AppBar
-    </nav>
-  );
+
+    return (
+        <header>
+            <Link to={`${PossibleRoutes.ROOT}`} className="title-link">
+                <h1 className="title">MiWi</h1>
+            </Link>
+            <div className="nav-options">
+                <Link to={`${PossibleRoutes.NEW_ENTRY}`} className="nav-link">
+                    New Entry
+                </Link>
+                <Link to={`${PossibleRoutes.ALL_ENTRIES}`} className="nav-link">
+                    All Entries
+                </Link>
+            </div>
+        </header>
+    );
 };
 
 export default AppBar;
