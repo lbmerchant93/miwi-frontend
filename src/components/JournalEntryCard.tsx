a01d4a625e52bb3dbd56032c4441478b01efc754
Lucas Merchant <lbmerchant93@gmail.com>
2022-02-25 15:33:27 -0700

    Add border and button container

--- src/components/JournalEntryCard.tsx
+++ src/components/JournalEntryCard.tsx
@@ -7,7 +7,7 @@ import './JournalEntryCard.css';
 
 const JournalEntryCard = () => {
   return (
-    <Box>
+    <Box className="journal-entry-card">
         <Typography variant="h5">Entry Date</Typography>
         <Typography variant="body1">Amount of water you drank: </Typography>
         <Typography variant="body1">Amount of protein you had: </Typography>
@@ -16,8 +16,11 @@ const JournalEntryCard = () => {
         <Typography variant="body1">How long you did garland pose for: </Typography>
         <Typography variant="body1">Did you take your prenatal vitamins? </Typography>
         <Typography variant="body1">Did you take your probiotics? </Typography>
-        <Button color="inherit" variant="outlined">Delete Entry</Button>
-        <Button color="inherit" variant="outlined">Edit Entry</Button>
+        <Box className="journal-entry-card-options">
+            <Button color="inherit" variant="outlined">Delete Entry</Button>
+            <Button color="inherit" variant="outlined">Edit Entry</Button> 
+        </Box>
+        
     </Box>
   )
 }
