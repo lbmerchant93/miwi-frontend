9a2816862575ca1f8dc9f55370aaadd7c8d56053
Lucas Merchant <lbmerchant93@gmail.com>
2022-02-25 15:29:58 -0700

    Create sample JournalEntryCard

--- src/components/JournalEntryCard.tsx
+++ src/components/JournalEntryCard.tsx
@@ -1,10 +1,24 @@
 import React from 'react';
-
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
 import './JournalEntryCard.css';
+
 
 const JournalEntryCard = () => {
   return (
-    <div>JournalEntryCard</div>
+    <Box>
+        <Typography variant="h5">Entry Date</Typography>
+        <Typography variant="body1">Amount of water you drank: </Typography>
+        <Typography variant="body1">Amount of protein you had: </Typography>
+        <Typography variant="body1">How long you exercised for: </Typography>
+        <Typography variant="body1">How many kegels you did: </Typography>
+        <Typography variant="body1">How long you did garland pose for: </Typography>
+        <Typography variant="body1">Did you take your prenatal vitamins? </Typography>
+        <Typography variant="body1">Did you take your probiotics? </Typography>
+        <Button color="inherit" variant="outlined">Delete Entry</Button>
+        <Button color="inherit" variant="outlined">Edit Entry</Button>
+    </Box>
   )
 }
 
