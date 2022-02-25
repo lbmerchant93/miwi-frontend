2ec58ddac86753e9d8fe52371fbb270c6a6db16e
Lucas Merchant <lbmerchant93@gmail.com>
2022-02-25 15:14:26 -0700

    Create JournalEntryCard file and css

---
+++ src/components/JournalEntryCard.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import './JournalEntryCard.css';
+
+const JournalEntryCard = () => {
+  return (
+    <div>JournalEntryCard</div>
+  )
+}
+
+export default JournalEntryCard;
