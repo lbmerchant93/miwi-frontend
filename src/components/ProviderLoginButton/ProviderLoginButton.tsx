02b5b7fad88a7427efbdd7dabe108f0aa1c68877
Lucas Merchant <lbmerchant93@gmail.com>
2022-02-27 14:53:07 -0700

    Create Provider Login button to log in through google

---
+++ src/components/ProviderLoginButton/ProviderLoginButton.tsx
@@ -0,0 +1,34 @@
+import React, { useEffect } from 'react';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+import Box from '@mui/material/Box';
+import GoogleIcon from '../../shared/Google.icon';
+import './ProviderLoginButton.css'
+
+interface ProviderLoginButtonsProps {
+    user: any;
+    onClose: () => void;
+}
+
+const ProviderLoginButton: React.FC<ProviderLoginButtonsProps> = (props) => {
+    const { user, onClose } = props;
+
+    useEffect(() => {
+        if (user.isLoggedIn) {
+            onClose()
+        }
+    }, [user, onClose])
+    
+    return (
+        <Box className="provider-login-button">
+            <Button variant="outlined" onClick={user.login} color="inherit" >
+                <GoogleIcon />
+                <Box className="provider-login-text-wrapper">
+                    <Typography variant="body1">Sign in with Google</Typography>
+                </Box>
+            </Button>
+        </Box>
+    )
+}
+
+export default ProviderLoginButton;
