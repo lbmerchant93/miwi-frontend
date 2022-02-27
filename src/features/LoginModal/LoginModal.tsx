623ac7af0b289f15ff85a7b05425d3276177de4c
Lucas Merchant <lbmerchant93@gmail.com>
2022-02-27 13:40:55 -0700

    Create centered LoginModal

--- src/features/LoginModal/LoginModal.tsx
+++ src/features/LoginModal/LoginModal.tsx
@@ -1,11 +1,33 @@
 import React from 'react';
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+import Modal from '@mui/material/Modal';
 
 import './LoginModal.css';
 
-const LoginModal = () => {
-  return (
-    <div>LoginModal</div>
-  )
-}
+interface LoginModalProps {
+    isOpen: boolean;
+    onClose: () => void;
+  }
 
-export default LoginModal
+const LoginModal: React.FC<LoginModalProps> = (props) => {
+    const { isOpen, onClose } = props;
+
+    return (
+        <Modal
+            open={isOpen}
+            onClose={onClose}
+            aria-labelledby="modal-modal-title"
+            aria-describedby="modal-modal-description"
+            className="login-modal"
+        >
+            <Box className="login-modal-container">
+                <Typography variant="h6" component="h2">
+                    Text in a modal
+                </Typography>
+            </Box>
+        </Modal>
+    )
+};
+
+export default LoginModal;
