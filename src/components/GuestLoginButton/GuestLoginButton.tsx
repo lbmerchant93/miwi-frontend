2e1fb4020aed266d05384e3f49d70a5ecfed9409
Lucas Merchant <lbmerchant93@gmail.com>
2022-03-02 12:39:36 -0700

    Create GuestLoginButton component folder

---
+++ src/components/GuestLoginButton/GuestLoginButton.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './GuestLoginButton.css';
+
+const GuestLoginButton = () => {
+  return (
+    <div>GuestLoginButton</div>
+  )
+};
+
+export default GuestLoginButton;
