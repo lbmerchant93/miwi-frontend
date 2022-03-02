7df08bbf25a57451a3cde67b1f37fa78b15fae13
Lucas Merchant <lbmerchant93@gmail.com>
2022-03-02 13:09:51 -0700

    Change email from test to guest

--- src/components/GuestLoginButton/GuestLoginButton.tsx
+++ src/components/GuestLoginButton/GuestLoginButton.tsx
@@ -14,7 +14,7 @@ const GuestLoginButton: React.FC<GuestLoginButtonsProps> = (props) => {
     
     return (
         <Box className="guest-login-button-wrapper">
-            <Button variant="outlined" onClick={() => user.loginWithEmail('test@test.com', 'testing')} color="inherit" className="guest-login-button">
+            <Button variant="outlined" onClick={() => user.loginWithEmail('guest@guest.com', 'guestguest')} color="inherit" className="guest-login-button">
                 <Box className="guest-login-text-wrapper">
                     <Typography variant="body1">Sign in as guest</Typography>
                 </Box>
