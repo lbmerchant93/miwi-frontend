bb809d83e07695dc7431288cbb5b541806b5dc8a
Lucas Merchant <lbmerchant93@gmail.com>
2022-03-08 12:50:56 -0700

    Create EditJournalEntryModal folder and file

---
+++ src/components/EditJournalEntryModal/EditJournalEntryModal.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import './EditJournalEntryModal.css';
+
+const EditJournalEntryModal = () => {
+
+    return (
+        <div>EditJournalEntryModal</div>
+    );
+};
+
+export default EditJournalEntryModal;
