3d235ab768911befff780fc9b942519e205e517b
Lucas Merchant <lbmerchant93@gmail.com>
2022-03-08 11:40:41 -0700

    Create initial WarningModal pieces

--- src/features/WarningModal/WarningModal.tsx
+++ src/features/WarningModal/WarningModal.tsx
@@ -1,12 +1,39 @@
 import React from 'react';
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+import Modal from '@mui/material/Modal';
+import Button from '@mui/material/Button';
 
 import './WarningModal.css';
 
-const WarningModal = () => {
+interface WarningModalProps {
+    isOpen: boolean;
+    onClose: () => void;
+    modalTitle: string;
+    modalDescription: string;
+    modalMessage: string;
+    verifiedAction: () => void;
+};
+
+const WarningModal: React.FC<WarningModalProps> = (props) => {
+    const { isOpen, onClose, modalTitle, modalDescription, modalMessage, verifiedAction } = props;
 
     return (
-        <div>WarningModal</div>
-    )
-}
+        <Modal
+            open={isOpen}
+            onClose={onClose}
+            aria-labelledby={`${modalTitle}`}
+            aria-describedby={`${modalDescription}`}
+            className="login-modal"
+        >
+            <Box>
+                <Typography variant="h5">{modalTitle}</Typography>
+                <Typography variant="body1">{modalMessage}</Typography>
+                <Button onClick={verifiedAction}>Yes I'm sure</Button>
+                <Button onClick={onClose}>No go back</Button>
+            </Box>
+        </Modal>
+    );
+};
 
 export default WarningModal;
