1d4d9839a52649199ec5e9110999d952a73d1eb4
Lucas Merchant <lbmerchant93@gmail.com>
2022-05-09 14:09:24 -0600

    Define height for keeping footer at bottom of page, add landing page content

--- src/pages/LandingPage/LandingPage.tsx
+++ src/pages/LandingPage/LandingPage.tsx
@@ -13,14 +13,14 @@ const MainPage:React.FC = () => {
           Welcome to MiWi
         </strong>
       </Typography>
-      <Typography variant="subtitle1">Your personal Midwifery App</Typography>
-      <Box className="landing-page-description-container">
+      <Typography variant="subtitle1">*Your personal Midwifery App*</Typography>
+      <Box className="landing-page-description-container" mt={4}>
+      <Typography variant='h5'><strong>Why MiWi?</strong></Typography>
+        <Typography variant="body1" mt={1} mb={4}>Preparing for your empowered, healthy birth and postpartum period takes focus. This journal guides you through the daily activities that will have you ready to rock your birth!</Typography>
         <Typography variant="h5"><strong>Reminder to give yourself some grace!</strong></Typography>
-        <Typography variant="body1">You're going to have bad days. You're going to skip some activities. Checking off everything every single day is not sustainable. Don't trash yourself. This journal isn't intended for a perfection destination. It is a tool to visually guide you through the journey.</Typography>
-      </Box>
-      <Box className="landing-page-description-container">
+        <Typography variant="body1" mt={1} mb={4}>You're going to have bad days. You're going to skip some activities. Checking off everything every single day is not sustainable. Don't trash yourself. This journal isn't intended for a perfection destination. It is a tool to visually guide you through the journey.</Typography>
         <Typography variant="h5"><strong>Whenever you're ready</strong></Typography>
-        <Typography variant="body1">Click the log in button at the top right corner of this page to sign in, where you will then be directed to your personal dashboard.</Typography>
+        <Typography variant="body1" mt={1}>Click the log in button at the top right corner of this page to sign in, where you will then be directed to your personal dashboard.</Typography>
       </Box>
     </Box>
   )
