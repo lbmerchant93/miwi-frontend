9db9c61dffac91eb895522de18fc03e9ce1684b6
Lucas Merchant <lbmerchant93@gmail.com>
2022-05-09 12:25:51 -0600

    Add basic content to about page

--- src/pages/AboutPage/AboutPage.tsx
+++ src/pages/AboutPage/AboutPage.tsx
@@ -1,10 +1,15 @@
 import React from 'react';
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
 
 import './AboutPage.css';
 
 const AboutPage = () => {
   return (
-    <div>AboutPage</div>
+    <Box className='about-page-container'>
+        <Typography variant='h2'><strong>Why MiWi?</strong></Typography>
+        <Typography variant="body1">Preparing for your empowered, healthy birth and postpartum period takes focus. This journal guides you through the daily activities that will have you ready to rock your birth!</Typography>
+    </Box>
   )
 }
 
