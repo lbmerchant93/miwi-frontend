752596df4a289493198f9226a548d504e500a2d7
Lucas Merchant <lbmerchant93@gmail.com>
2022-05-19 12:50:28 -0600

    Create test for header, main, footer

--- cypress/integration/landing_page_spec.ts
+++ cypress/integration/landing_page_spec.ts
@@ -1,7 +1,13 @@
 /// <reference types="cypress" />
 
-describe('The Home Page', () => {
-    it('successfully loads', () => {
-      cy.visit('/')
-    })
+describe('Landing Page', () => {
+  beforeEach(() => {
+    cy.visit('/')
+  })
+
+  it('Should successfully load page, containing an AppBar header, main content container, and an AppFooter footer.', () => {
+    cy.get('header').contains('MiWi').should('be.visible')
+    cy.get('main').contains('Welcome to MiWi').should('be.visible')
+    cy.get('footer').contains('Support').should('be.visible')
+  })
 })
