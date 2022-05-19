bc1dfa7266468fc559647b853a34e3aea4334d91
Lucas Merchant <lbmerchant93@gmail.com>
2022-05-19 11:01:35 -0600

    Add Lucas profile info

--- src/pages/AboutPage/AboutPage.tsx
+++ src/pages/AboutPage/AboutPage.tsx
@@ -1,15 +1,30 @@
 import React from 'react';
 import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
+import Avatar from '@mui/material/Avatar';
+import lucasProfile from '../../images/lucasProfile.jpg';
+import Link from '@mui/material/Link';
 
 import './AboutPage.css';
 
 const AboutPage = () => {
   return (
-    <Box className='about-page-container'>
-        <Typography variant='h2'><strong>Why MiWi?</strong></Typography>
-        <Typography variant="body1">Preparing for your empowered, healthy birth and postpartum period takes focus. This journal guides you through the daily activities that will have you ready to rock your birth!</Typography>
-    </Box>
+    <>
+      <Box className='about-page-container'>
+          <Typography variant='h2' mb={2}><strong>Meet Our Team</strong></Typography>
+          <Box className='team-container' mb={4}>
+            <Typography variant="h4">Lucas Merchant</Typography>
+            <Typography variant="body1" mb={1}>Software Developer</Typography>
+            <Avatar
+              src={lucasProfile}
+              alt="Lucas Profile Picture"
+              style={{ fontSize: '100px', height: 200, width: 200, marginBottom: '8px' }}>
+            </Avatar>
+            <Link href="https://github.com/lbmerchant93" underline="always" variant="body1">Github: lbmerchant93</Link>
+            <Link href="https://www.linkedin.com/in/lucas-merchant93/" underline="always" variant="body1">LinkedIn: lbmerchant93</Link>
+          </Box>
+      </Box>
+    </>
   )
 }
 
