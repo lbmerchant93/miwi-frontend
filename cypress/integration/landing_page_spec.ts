d3c095faf21ef86131d0ff5a9a95fa6eb09b99b1
Lucas Merchant <lbmerchant93@gmail.com>
2022-05-23 13:05:49 -0600

    Add intercepts for AggregateJournalEntry and JournalEntries queries

--- cypress/integration/landing_page_spec.ts
+++ cypress/integration/landing_page_spec.ts
@@ -36,7 +36,102 @@ describe('AppBar', () => {
 
       // Mutations
       aliasMutation(req, 'loginUser')
-      const { body } = req
+
+      if (hasOperationName(req, 'User')) {
+        req.alias = 'gqlUserQuery'
+
+        req.reply((res) => {
+          res.body.data.user.id = "opov7wBC6RPiEnJP6hB8iiCtPeq1"
+          res.body.data.user.displayName = "Guest Viewer"
+          res.body.data.user.email = "guest@guest.com"
+          res.body.data.user.expectedDueDate = '2022-07-01T00:00:00.000-06:00'
+        })
+      }
+
+      if(hasOperationName(req, 'AggregateJournalEntry')) {
+        req.alias = 'gqlAggregateJournalEntryQuery'
+
+        req.reply((res) => {
+          res.body.data.aggregateJournalEntry._count._all = 6
+        })
+      }
+
+      if(hasOperationName(req, 'JournalEntries')) {
+        req.alias = 'gqlJournalEntriesQuery'
+
+        req.reply((res) => {
+          res.body.data.journalEntries = [
+            {
+              authorId: "opov7wBC6RPiEnJP6hB8iiCtPeq1",
+              date: "2022-05-01T00:00:00.000-06:00",
+              exercise: 1,
+              garlandPose: 1,
+              id: 1,
+              kegels: 1,
+              prenatalVitamins: true,
+              probiotics: true,
+              proteinIntake: 1,
+              waterIntake: 1
+            }, {
+              authorId: "opov7wBC6RPiEnJP6hB8iiCtPeq1",
+              date: "2022-05-02T00:00:00.000-06:00",
+              exercise: 2,
+              garlandPose: 2,
+              id: 2,
+              kegels: 2,
+              prenatalVitamins: false,
+              probiotics: false,
+              proteinIntake: 2,
+              waterIntake: 2
+            }, {
+              authorId: "opov7wBC6RPiEnJP6hB8iiCtPeq1",
+              date: "2022-05-03T00:00:00.000-06:00",
+              exercise: 3,
+              garlandPose: 3,
+              id: 3,
+              kegels: 3,
+              prenatalVitamins: true,
+              probiotics: true,
+              proteinIntake: 3,
+              waterIntake: 3
+            }, {
+              authorId: "opov7wBC6RPiEnJP6hB8iiCtPeq1",
+              date: "2022-05-04T00:00:00.000-06:00",
+              exercise: 4,
+              garlandPose: 4,
+              id: 4,
+              kegels: 4,
+              prenatalVitamins: false,
+              probiotics: true,
+              proteinIntake: 4,
+              waterIntake: 4
+            }, {
+              authorId: "opov7wBC6RPiEnJP6hB8iiCtPeq1",
+              date: "2022-05-05T00:00:00.000-06:00",
+              exercise: 5,
+              garlandPose: 5,
+              id: 5,
+              kegels: 5,
+              prenatalVitamins: true,
+              probiotics: false,
+              proteinIntake: 5,
+              waterIntake: 5
+            }, {
+              authorId: "opov7wBC6RPiEnJP6hB8iiCtPeq1",
+              date: "2022-05-06T00:00:00.000-06:00",
+              exercise: 6,
+              garlandPose: 6,
+              id: 6,
+              kegels: 6,
+              prenatalVitamins: true,
+              probiotics: true,
+              proteinIntake: 6,
+              waterIntake: 6
+            },
+          ]
+        })
+      }
+
       if (hasOperationName(req, 'loginUser')) {
         req.alias = 'gqlloginUserMutation'
 
@@ -70,14 +165,19 @@ describe('AppBar', () => {
     cy.get('.login-form #Password').eq(0).type('guestviewer')
     cy.get('.login-form #Password').eq(0).should('have.attr', 'value', 'guestviewer')
     cy.get('.login-form-button button').click()
-    cy.wait('@gqlUserQuery')
     cy.wait('@gqlloginUserMutation').its('response.body.data.loginUser').should((loginUser) => {
-      // Add other response property test here
       expect(loginUser.displayName).to.be.equal('Guest Viewer')
       expect(loginUser.email).to.be.equal('guest@guest.com')
       expect(loginUser.expectedDueDate).to.be.equal('2022-07-01T00:00:00.000-06:00')
     })
-    cy.wait('@gqlAggregateJournalEntryQuery')
+    cy.wait('@gqlUserQuery').its('response.body.data.user').should((user) => {
+      expect(user.displayName).to.be.equal('Guest Viewer')
+      expect(user.email).to.be.equal('guest@guest.com')
+      expect(user.expectedDueDate).to.be.equal('2022-07-01T00:00:00.000-06:00')
+    })
+    cy.wait('@gqlAggregateJournalEntryQuery').its('response.body.data.aggregateJournalEntry').should((aggregateJournalEntry) => {
+      expect(aggregateJournalEntry._count._all).to.be.equal(6)
+    })
     cy.wait('@gqlJournalEntriesQuery')
   })
 })
