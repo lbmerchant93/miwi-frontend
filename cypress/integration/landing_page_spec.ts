60ec5c42604123fd08bb116bc85ad354bc0265f8
Lucas Merchant <lbmerchant93@gmail.com>
2022-06-13 14:49:55 -0600

    Move footer to separate spec

--- cypress/integration/landing_page_spec.ts
+++ cypress/integration/landing_page_spec.ts
@@ -243,25 +243,3 @@ describe('AppBar', () => {
     cy.get('header .login-button button').contains('Log In')
   })
 })
-
-describe('AppFooter', () => {
-  beforeEach(() => {
-    cy.visit('/')
-  })
-
-  it('Should have a links for About Us and How it works', () => {
-    cy.get('footer a').should('have.length', 2)
-    cy.get('footer a').eq(0).contains('About Us').should('be.visible')
-    cy.get('footer a').eq(1).contains('How MiWi Works').should('be.visible')
-  })
-
-  it('Should navigate to the About Us page on About Us link click', () => {
-    cy.get('footer a').eq(0).click()
-    cy.url().should('equal', 'http://localhost:3000/about')
-  })
-
-  it('Should navigate to the How MiWi Works page on How MiWi Works link click', () => {
-    cy.get('footer a').eq(1).click()
-    cy.url().should('equal', 'http://localhost:3000/how_miwi_works')
-  })
-})
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,25 +1,132 @@
-// ***********************************************
-// This example commands.js shows you how to
-// create various custom commands and overwrite
-// existing commands.
-//
-// For more comprehensive examples of custom
-// commands please read more here:
-// https://on.cypress.io/custom-commands
-// ***********************************************
-//
-//
-// -- This is a parent command --
-// Cypress.Commands.add('login', (email, password) => { ... })
-//
-//
-// -- This is a child command --
-// Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
-//
-//
-// -- This is a dual command --
-// Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
-//
-//
-// -- This will overwrite an existing command --
-// Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+/* eslint-disable no-undef */
+import { hasOperationName, aliasQuery, aliasMutation } from '../utils/graphql-test-utils';
+
+Cypress.Commands.add('login', () => {
+    cy.intercept('POST', 'http://localhost:9000/graphql', (req) => {
+      // Queries
+      aliasQuery(req, 'User')
+      aliasQuery(req, 'AggregateJournalEntry')
+      aliasQuery(req, 'JournalEntries')
+
+      // Mutations
+      aliasMutation(req, 'loginUser')
+
+      if (hasOperationName(req, 'User')) {
+        req.alias = 'gqlUserQuery'
+
+        req.reply((res) => {
+          res.body.data.user.id = "opov7wBC6RPiEnJP6hB8iiCtPeq1"
+          res.body.data.user.displayName = "Guest Viewer"
+          res.body.data.user.email = "guest@guest.com"
+          res.body.data.user.expectedDueDate = '2022-07-01T00:00:00.000-06:00'
+        })
+      }
+
+      if(hasOperationName(req, 'AggregateJournalEntry')) {
+        req.alias = 'gqlAggregateJournalEntryQuery'
+
+        req.reply((res) => {
+          res.body.data.aggregateJournalEntry._count._all = 6
+        })
+      }
+
+      if(hasOperationName(req, 'JournalEntries')) {
+        req.alias = 'gqlJournalEntriesQuery'
+
+        req.reply((res) => {
+          res.body.data.journalEntries = [
+            {
+              authorId: "opov7wBC6RPiEnJP6hB8iiCtPeq1",
+              date: "2022-05-01T00:00:00.000-06:00",
+              exercise: 1,
+              garlandPose: 1,
+              id: 1,
+              kegels: 1,
+              prenatalVitamins: true,
+              probiotics: true,
+              proteinIntake: 1,
+              waterIntake: 1
+            }, {
+              authorId: "opov7wBC6RPiEnJP6hB8iiCtPeq1",
+              date: "2022-05-02T00:00:00.000-06:00",
+              exercise: 2,
+              garlandPose: 2,
+              id: 2,
+              kegels: 2,
+              prenatalVitamins: false,
+              probiotics: false,
+              proteinIntake: 2,
+              waterIntake: 2
+            }, {
+              authorId: "opov7wBC6RPiEnJP6hB8iiCtPeq1",
+              date: "2022-05-03T00:00:00.000-06:00",
+              exercise: 3,
+              garlandPose: 3,
+              id: 3,
+              kegels: 3,
+              prenatalVitamins: true,
+              probiotics: true,
+              proteinIntake: 3,
+              waterIntake: 3
+            }, {
+              authorId: "opov7wBC6RPiEnJP6hB8iiCtPeq1",
+              date: "2022-05-04T00:00:00.000-06:00",
+              exercise: 4,
+              garlandPose: 4,
+              id: 4,
+              kegels: 4,
+              prenatalVitamins: false,
+              probiotics: true,
+              proteinIntake: 4,
+              waterIntake: 4
+            }, {
+              authorId: "opov7wBC6RPiEnJP6hB8iiCtPeq1",
+              date: "2022-05-05T00:00:00.000-06:00",
+              exercise: 5,
+              garlandPose: 5,
+              id: 5,
+              kegels: 5,
+              prenatalVitamins: true,
+              probiotics: false,
+              proteinIntake: 5,
+              waterIntake: 5
+            }, {
+              authorId: "opov7wBC6RPiEnJP6hB8iiCtPeq1",
+              date: "2022-05-06T00:00:00.000-06:00",
+              exercise: 6,
+              garlandPose: 6,
+              id: 6,
+              kegels: 6,
+              prenatalVitamins: true,
+              probiotics: true,
+              proteinIntake: 6,
+              waterIntake: 6
+            },
+          ]
+        })
+      }
+
+      if (hasOperationName(req, 'loginUser')) {
+        req.alias = 'gqlloginUserMutation'
+
+        req.reply((res) => {
+          res.body.data.loginUser.id = "opov7wBC6RPiEnJP6hB8iiCtPeq1"
+          res.body.data.loginUser.displayName = "Guest Viewer"
+          res.body.data.loginUser.email = "guest@guest.com"
+          res.body.data.loginUser.expectedDueDate = '2022-07-01T00:00:00.000-06:00'
+        })
+      }
+    })
+    cy.visit('/')
+
+    cy.get('header .login-button button').click()
+    cy.get('.login-form #Email').eq(0).type('guest@guest.com')
+    cy.get('.login-form #Password').eq(0).type('guestviewer')
+    cy.get('.login-form-button button').click()
+    
+})
+
+Cypress.Commands.add('logout', () => {
+    cy.get('#menu-button').click()
+    cy.get('.logout-link').click()
+})
