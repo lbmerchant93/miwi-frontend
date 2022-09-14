5066a68b88e9f9a25ba0666d5230962c7fae9106
Lucas Merchant <lbmerchant93@gmail.com>
2022-09-14 13:13:04 -0600

    Create placeholder for mood section

--- src/features/JournalEntryDisplay/JournalEntryDisplay.tsx
+++ src/features/JournalEntryDisplay/JournalEntryDisplay.tsx
@@ -3,6 +3,11 @@ import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
 import CheckBoxOutlineBlankIcon from '@mui/icons-material/CheckBoxOutlineBlank';
 import CheckBoxIcon from '@mui/icons-material/CheckBox';
+import SentimentSatisfiedAltIcon from '@mui/icons-material/SentimentSatisfiedAlt';
+import SentimentSatisfiedIcon from '@mui/icons-material/SentimentSatisfied';
+import SentimentNeutralIcon from '@mui/icons-material/SentimentNeutral';
+import SentimentDissatisfiedIcon from '@mui/icons-material/SentimentDissatisfied';
+import SentimentVeryDissatisfiedIcon from '@mui/icons-material/SentimentVeryDissatisfied';
 
 interface JournalEntryDisplayProps {
 
@@ -13,19 +18,19 @@ const JournalEntryDisplay: React.FC<JournalEntryDisplayProps> = (props) => {
         <Box width={"100%"} display="flex" flexDirection="column" mt={7}>
             <Box display="flex" flexDirection="row" flexWrap="wrap" justifyContent="space-around">
                 <Box>
-                    <Typography variant="body1">Water</Typography>
+                    <Typography variant="body1"><strong>Water</strong></Typography>
                     <Box border={"1px solid black"} borderRadius={"50%"} height={200} width={200}>
                         <Typography variant="body1">100%</Typography>
                     </Box>
                 </Box>
                 <Box>
-                    <Typography variant="body1">Protein</Typography>
+                    <Typography variant="body1"><strong>Protein</strong></Typography>
                     <Box border={"1px solid black"} borderRadius={"50%"} height={200} width={200}>
                         <Typography variant="body1">100%</Typography>
                     </Box>
                 </Box>
                 <Box>
-                    <Typography variant="body1">Exercise</Typography>
+                    <Typography variant="body1"><strong>Exercise</strong></Typography>
                     <Box border={"1px solid black"} borderRadius={"50%"} height={200} width={200}>
                         <Typography variant="body1">100%</Typography>
                     </Box>
@@ -33,13 +38,13 @@ const JournalEntryDisplay: React.FC<JournalEntryDisplayProps> = (props) => {
             </Box>    
             <Box display="flex" flexDirection="row" flexWrap="wrap" justifyContent="space-evenly" mt={7}>
                 <Box>
-                    <Typography variant="body1">Kegels</Typography>
+                    <Typography variant="body1"><strong>Kegels</strong></Typography>
                     <Box border={"1px solid black"} borderRadius={"50%"} height={200} width={200}>
                         <Typography variant="body1">100%</Typography>
                     </Box>
                 </Box>
                 <Box>
-                    <Typography variant="body1">Garland Pose</Typography>
+                    <Typography variant="body1"><strong>Garland Pose</strong></Typography>
                     <Box border={"1px solid black"} borderRadius={"50%"} height={200} width={200}>
                         <Typography variant="body1">100%</Typography>
                     </Box>
@@ -53,13 +58,14 @@ const JournalEntryDisplay: React.FC<JournalEntryDisplayProps> = (props) => {
                 justifyContent="space-around" 
                 alignItems="center" 
                 height={90}
+                mx={3}
             >
                 <Box display="flex" flexDirection="row">
-                    <Typography variant="body1" pr={2}>Vitamins</Typography>
+                    <Typography variant="body1" pr={2}><strong>Vitamins</strong></Typography>
                     <CheckBoxIcon />
                 </Box>
                 <Box display="flex" flexDirection="row">
-                    <Typography variant="body1" pr={2}>Probiotics</Typography>
+                    <Typography variant="body1" pr={2}><strong>Probiotics</strong></Typography>
                     <CheckBoxOutlineBlankIcon />
                 </Box>
             </Box>    
@@ -94,10 +100,23 @@ const JournalEntryDisplay: React.FC<JournalEntryDisplayProps> = (props) => {
                     </Box>
                 </Box>
             </Box>
-
-            
-            
-            <Box>Mood</Box>
+            <Box
+                border={"1px solid black"} 
+                my={7} 
+                display="flex" 
+                flexDirection="column" 
+                height={90}
+                mx={3}
+            >
+                <Typography variant="body1" mb={1}><strong>Mood</strong></Typography>
+                <Box display="flex" flexDirection="row" justifyContent="space-around">
+                    <SentimentVeryDissatisfiedIcon fontSize="large" />
+                    <SentimentDissatisfiedIcon fontSize="large" />
+                    <SentimentNeutralIcon fontSize="large" />
+                    <SentimentSatisfiedIcon fontSize="large" />
+                    <SentimentSatisfiedAltIcon fontSize="large" color="success"/>
+                </Box>
+            </Box>
         </Box>
     );
 };
