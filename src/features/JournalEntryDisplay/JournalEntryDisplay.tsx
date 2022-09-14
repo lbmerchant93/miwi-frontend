3cd89b977bfaf459ae5556bfecbae0e05758be0d
Lucas Merchant <lbmerchant93@gmail.com>
2022-09-14 11:28:05 -0600

    Create initial placeholders for Graphs

--- src/features/JournalEntryDisplay/JournalEntryDisplay.tsx
+++ src/features/JournalEntryDisplay/JournalEntryDisplay.tsx
@@ -1,8 +1,55 @@
 import React from 'react';
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
 
-const JournalEntryDisplay = () => {
+interface JournalEntryDisplayProps {
+
+}
+
+const JournalEntryDisplay: React.FC<JournalEntryDisplayProps> = (props) => {
     return (
-        <div>JournalEntryDisplay</div>
+        <Box width={"100%"} display="flex" flexDirection="column" mt={3}>
+            <Box display="flex" flexDirection="row" flexWrap="wrap" justifyContent="space-around">
+                <Box>
+                    <Typography variant="body1">Water</Typography>
+                    <Box border={"1px solid black"} borderRadius={"50%"} height={200} width={200}>
+                        <Typography variant="body1">100%</Typography>
+                    </Box>
+                </Box>
+                <Box>
+                    <Typography variant="body1">Protein</Typography>
+                    <Box border={"1px solid black"} borderRadius={"50%"} height={200} width={200}>
+                        <Typography variant="body1">100%</Typography>
+                    </Box>
+                </Box>
+                <Box>
+                    <Typography variant="body1">Exercise</Typography>
+                    <Box border={"1px solid black"} borderRadius={"50%"} height={200} width={200}>
+                        <Typography variant="body1">100%</Typography>
+                    </Box>
+                </Box>
+            </Box>    
+            <Box display="flex" flexDirection="row" flexWrap="wrap" justifyContent="space-evenly">
+                <Box>
+                    <Typography variant="body1">Kegels</Typography>
+                    <Box border={"1px solid black"} borderRadius={"50%"} height={200} width={200}>
+                        <Typography variant="body1">100%</Typography>
+                    </Box>
+                </Box>
+                <Box>
+                    <Typography variant="body1">Garland Pose</Typography>
+                    <Box border={"1px solid black"} borderRadius={"50%"} height={200} width={200}>
+                        <Typography variant="body1">100%</Typography>
+                    </Box>
+                </Box>
+            </Box>
+                
+            
+
+            <Box>Check list</Box>
+            <Box>Writing</Box>
+            <Box>Mood</Box>
+        </Box>
     );
 };
 
--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,11 +1,20 @@
-import React from 'react';
-import { useParams } from 'react-router-dom';
+import React, { useContext } from 'react';
+// import { useParams } from 'react-router-dom';
+import { AuthContext } from '../../shared/auth-context';
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+import JournalEntryDisplay from '../../features/JournalEntryDisplay/JournalEntryDisplay';
+
 
 const HomePage = () => {
-    const { user } = useParams();
+    // const { user } = useParams();
+    const user = useContext(AuthContext);
     
     return (
-        <div>HomePage</div>
+        <Box width={'100%'} display="flex" flexDirection="column" textAlign="center">
+            <Typography variant="h4"><strong>Today's Journal Entry</strong></Typography>
+            <JournalEntryDisplay />
+        </Box>
     );
 }
 
