16b8595333245b8a8e0b17430a840a4b0ff6f606
Lucas Merchant <lbmerchant93@gmail.com>
2022-09-28 12:26:18 -0600

    Align JournalEntryCard with just the entry's date

--- src/components/JournalEntryCard/JournalEntryCard.tsx
+++ src/components/JournalEntryCard/JournalEntryCard.tsx
@@ -3,30 +3,19 @@ import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
 import moment from 'moment';
 import { JournalEntry } from '../../pages/HomePage/HomePage';
-import './JournalEntryCard.css';
 
 interface JournalEntryCardProps {
   entry: JournalEntry;
-}
+};
 
 const JournalEntryCard: React.FC<JournalEntryCardProps> = (props) => {
   const { entry } = props;
 
   return (
-    <>
-      <Box className="journal-entry-card"> 
-        <Typography variant="h5"><b>{moment(entry.date).format("MMMM Do YYYY")}</b></Typography>
-        <Typography variant="body1">You drank <b>{entry.waterIntake}oz</b> of water</Typography>
-        <Typography variant="body1">You had <b>{entry.proteinIntake}g</b> of protein</Typography>
-        <Typography variant="body1">You exercised for <b>{entry.exercise} minute{entry.exercise === 1 ? '' : 's'}</b></Typography>
-        <Typography variant="body1">You did <b>{entry.kegels} kegel{entry.kegels === 1 ? '' : 's'}</b></Typography>
-        <Typography variant="body1">You did garland pose for <b>{entry.garlandPose} minute{entry.garlandPose === 1 ? '' : 's'}</b></Typography>
-        <Typography variant="body1">You <b>{entry.prenatalVitamins ? 'did' : 'did not'}</b> take your prenatal vitamins </Typography>
-        <Typography variant="body1">You <b>{entry.probiotics ? 'did' : 'did not'}</b> take your probiotics </Typography>
-      </Box>
-    </>
-    
-  )
-}
+    <Box border={'1px solid grey'} borderRadius={5} p={2} width={200} height={150} m={2} display={"flex"} alignItems={"center"}> 
+      <Typography variant="h5"><b>{moment(entry.date).format("dddd, MMMM Do YYYY ")}</b></Typography>
+    </Box>
+  );
+};
 
 export default JournalEntryCard;
