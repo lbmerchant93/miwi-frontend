91850128f3b8d9593ba764ae0ed890e4862fabd4
Lucas Merchant <lbmerchant93@gmail.com>
2022-09-29 12:28:32 -0600

    Create JournalEntryPage and route

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -6,5 +6,6 @@ export enum PossibleRoutes {
     HOW_IT_WORKS = '/how_miwi_works',
     HOME_USER = '/home/:user',
     PROFILE_USER = '/profile/:user',
-    JOURNAL_USER = '/journal/:user',
+    JOURNAL_ENTRIES = '/journal/entries',
+    JOURNAL_ENTRIES_ENTRY = '/journal/entries/:entry'
 }
