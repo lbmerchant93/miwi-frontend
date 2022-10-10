a51fe30b335412a1ef585205ac25f43f518b684e
Lucas Merchant <lbmerchant93@gmail.com>
2022-10-10 12:05:19 -0600

    Create Journal Entry Display skeleton and call on Home Page and Journal Entry Page

--- src/features/JournalEntryDisplay/JournalEntryDisplayLoading.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const JournalEntryDisplayLoading = () => {
-    return (
-        <div>JournalEntryDisplayLoading</div>
-    );
-};
-
-export default JournalEntryDisplayLoading;
