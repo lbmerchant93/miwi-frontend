72ff22dd6ec795d0e6587b379c94dc8616035b34
Lucas Merchant <lbmerchant93@gmail.com>
2022-10-11 13:12:12 -0600

    Remove old HowMiWiWorksPage components

--- src/pages/HowMiWiWorks/HowMiWiWorksDashboard.tsx
+++ src/pages/HowMiWiWorks/HowMiWiWorksDashboard.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import Box from '@mui/material/Box';
-import HowMiWiWorksPanel from './HowMiWiWorksTabbedPanel/HowMiWiWorksTabbedPanel';
+import HowMiWiWorksPanel from './HowMiWiWorksPanel/HowMiWiWorksPanel';
 
 const HowMiWiWorksDashboard = () => {
     return (
