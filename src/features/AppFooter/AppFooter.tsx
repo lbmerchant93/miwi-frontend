6e26e5681311551d7cfb96f49399fdcbf5a7377e
Lucas Merchant <lbmerchant93@gmail.com>
2022-10-17 12:13:35 -0600

    Fix footer width

--- src/features/AppFooter/AppFooter.tsx
+++ src/features/AppFooter/AppFooter.tsx
@@ -10,17 +10,19 @@ const AppFooter: React.FC = () => {
 
   return (
     <footer>
-      <Box className="footer-links-container">
-        <Typography variant="body1" mb={1} color="primary.light">Support</Typography>
-        <Link to={`${PossibleRoutes.ABOUT}`} className="footer-link">
-            <Typography variant="body1">About Us</Typography>
-        </Link>
-        <Link to={`${PossibleRoutes.HOW_MIWI_WORKS}`} className="footer-link">
-            <Typography variant="body1">How MiWi Works</Typography>
-        </Link>
-        {/* <Link to={`${PossibleRoutes.ROOT}`} className="footer-link">
-            <Typography variant="body1">Contact Us</Typography>
-        </Link> */}
+      <Box maxWidth="1232px" width={"100%"} display="flex">
+        <Box display="flex" flexDirection="column" alignItems="flex-start" py={3} px={2}>
+          <Typography variant="body1" mb={1} color="primary.light">Support</Typography>
+          <Link to={`${PossibleRoutes.ABOUT}`} className="footer-link">
+              <Typography variant="body1">About Us</Typography>
+          </Link>
+          <Link to={`${PossibleRoutes.HOW_MIWI_WORKS}`} className="footer-link">
+              <Typography variant="body1">How MiWi Works</Typography>
+          </Link>
+          {/* <Link to={`${PossibleRoutes.ROOT}`} className="footer-link">
+              <Typography variant="body1">Contact Us</Typography>
+          </Link> */}
+        </Box>
       </Box>
     </footer>
   );
