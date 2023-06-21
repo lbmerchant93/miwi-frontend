34ab22c85fa84ea870d1357fafcc4bfa044766f6
Lucas Merchant <lbmerchant93@gmail.com>
2023-06-21 12:45:52 -0600

    Change from box-shadow on footer to divider

--- src/features/AppFooter/AppFooter.styled.ts
+++ src/features/AppFooter/AppFooter.styled.ts
@@ -2,7 +2,6 @@ import styled from 'styled-components/macro';
 import { Link } from 'react-router-dom';
 
 export const Footer = styled.footer`
-    box-shadow: 0px -4px 7px -4px rgb(0 0 0 / 20%);
     min-height: 5em;
     display: flex;
     flex-direction: column;
