fd4e1734bc40dbe56653719e7665de238c4a0799
Lucas Merchant <lbmerchant93@gmail.com>
2023-06-21 13:00:20 -0600

    Transition AboutPage from css to styled components

---
+++ src/pages/AboutPage/AboutPage.styled.ts
@@ -0,0 +1,9 @@
+import styled from 'styled-components/macro';
+import Box from '@mui/material/Box';
+
+export const AboutPageContainer = styled(Box)`
+    display: flex;
+    flex-direction: column;
+    text-align: center;
+    align-items: center;
+`;
