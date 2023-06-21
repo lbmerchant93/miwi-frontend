58701288a5b7a90735963d6c83c712fef4002e13
Lucas Merchant <lbmerchant93@gmail.com>
2023-06-21 12:42:14 -0600

    Transition from css to styled components, AppFooter.

--- src/features/AppFooter/AppFooter.styled.ts
+++ src/features/AppFooter/AppFooter.styled.ts
@@ -1,11 +1,14 @@
-footer {
+import styled from 'styled-components/macro';
+import { Link } from 'react-router-dom';
+
+export const Footer = styled.footer`
     box-shadow: 0px -4px 7px -4px rgb(0 0 0 / 20%);
     min-height: 5em;
     display: flex;
     flex-direction: column;
     align-items: center;
-}
+`;
 
-.footer-link {
+export const FooterLink = styled(Link)`
     color: rgb(0 0 0 );
-}
+`;
--- src/features/AppFooter/AppFooter.tsx
+++ src/features/AppFooter/AppFooter.tsx
@@ -1,30 +1,28 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 import { PossibleRoutes } from '../../utils/constants';
 import Typography from '@mui/material/Typography';
 import Box from '@mui/material/Box';
-
-import './AppFooter.css';
+import { Footer, FooterLink } from './AppFooter.styled';
 
 const AppFooter: React.FC = () => {
 
   return (
-    <footer>
+    <Footer>
       <Box maxWidth="1232px" width={"100%"} display="flex">
         <Box display="flex" flexDirection="column" alignItems="flex-start" py={3} px={2}>
           <Typography variant="body1" mb={1} color="primary.light">Support</Typography>
-          <Link to={`${PossibleRoutes.ABOUT}`} className="footer-link">
+          <FooterLink to={`${PossibleRoutes.ABOUT}`}>
               <Typography variant="body1">About Us</Typography>
-          </Link>
-          <Link to={`${PossibleRoutes.HOW_MIWI_WORKS}`} className="footer-link">
+          </FooterLink>
+          <FooterLink to={`${PossibleRoutes.HOW_MIWI_WORKS}`}>
               <Typography variant="body1">How MiWi Works</Typography>
-          </Link>
-          {/* <Link to={`${PossibleRoutes.ROOT}`} className="footer-link">
+          </FooterLink>
+          {/* <FooterLink to={`${PossibleRoutes.ROOT}`}>
               <Typography variant="body1">Contact Us</Typography>
-          </Link> */}
+          </FooterLink> */}
         </Box>
       </Box>
-    </footer>
+    </Footer>
   );
 };
 
