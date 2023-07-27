dc8764b814a6bdd94b71bbc83388348599d4fb6e
Lucas Merchant <lbmerchant93@gmail.com>
2023-07-27 13:32:35 -0600

    Move some props to styled component

--- src/pages/LandingPage/LandingPage.styled.ts
+++ src/pages/LandingPage/LandingPage.styled.ts
@@ -9,11 +9,22 @@ export const LandingPageContainer = styled(Box)`
 `;
 
 export const LandingPageDescriptionContainer = styled(Box)`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    background-color: #6d0f93;
+    color: white;
+    align-self: center;
     border: rgb(0 0 0 / 20%) solid 1px;
     box-shadow: -11px 11px 3px -4px rgb(0 0 0 / 20%);
 `;
 
 export const LandingPageDescriptionMiddleContainer = styled(Box)`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    color: #6d0f93;
+    align-self: center;
     border: rgb(0 0 0 / 20%) solid 1px;
     box-shadow: 11px 11px 7px -4px rgb(0 0 0 / 20%);
 `;
--- src/pages/LandingPage/LandingPage.tsx
+++ src/pages/LandingPage/LandingPage.tsx
@@ -32,7 +32,7 @@ const MainPage:React.FC = () => {
       </Box>
       <LandingPageContainer mt={5} color="#6d0f93">
         <Box display="flex" justifyContent="center">
-          <LandingPageDescriptionContainer display="flex" flexDirection="column" justifyContent="center" px={2} py={2} bgcolor="#6d0f93" color="white" borderRadius={10} minHeight={200} maxWidth={750} alignSelf="center">
+          <LandingPageDescriptionContainer px={2} py={2} borderRadius={10} minHeight={200} maxWidth={750}>
             <Typography variant='h4'><strong>Why MiWi?</strong></Typography>
             <Typography variant="h6" mt={1}>Preparing for your empowered, healthy birth and postpartum period takes focus. This journal guides you through the daily activities that will have you ready to rock your birth!</Typography>
           </LandingPageDescriptionContainer>
@@ -40,13 +40,13 @@ const MainPage:React.FC = () => {
         </Box>
         <Box display="flex" justifyContent="center" mt={5}>
           {!isTablet && <Box display="flex" alignItems="center"><img width="250" height="250" src="https://img.icons8.com/color/250/flying-stork-with-bundle.png" alt="flying-stork-with-bundle"/></Box>}
-          <LandingPageDescriptionMiddleContainer display="flex" flexDirection="column" justifyContent="center" px={2} py={2} color="#6d0f93" borderRadius={10} minHeight={200} maxWidth={750} alignSelf="center" mt={2}>
+          <LandingPageDescriptionMiddleContainer px={2} py={2} borderRadius={10} minHeight={200} maxWidth={750} mt={2}>
             <Typography variant="h4"><strong>Remember to allow yourself grace!</strong></Typography>
             <Typography variant="h6" mt={1}>You're going to have bad days. You're going to skip some activities. Checking off everything every single day is not sustainable. Don't trash yourself. This journal isn't intended for a perfection destination. It is a tool to visually guide you through the journey. While journaling try to think: <i>"What is something that made me laugh or made me grateful today?"</i></Typography>
           </LandingPageDescriptionMiddleContainer>
         </Box>
-        <Box display="flex" justifyContent="center" mt={3}>
-          <LandingPageDescriptionContainer display="flex" flexDirection="column" justifyContent="center" px={2} py={2} bgcolor="#6d0f93" color="white" borderRadius={10} minHeight={200} maxWidth={750} alignSelf="center" mt={2}>
+        <Box display="flex" justifyContent="center" mt={5}>
+          <LandingPageDescriptionContainer px={2} py={2} borderRadius={10} minHeight={200} maxWidth={750} mt={2}>
             <Typography variant="h4"><strong>Whenever you're ready</strong></Typography>
             <Typography variant="h6" mt={1}>Click the log in button at the top right corner of this page to sign in, where you will then be directed to your personal dashboard.</Typography>
           </LandingPageDescriptionContainer> 
