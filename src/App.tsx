c61090975edeb5fdae20d45b9e8383ee5ff80ad7
Lucas Merchant <lbmerchant93@gmail.com>
2023-08-21 14:08:44 -0600

    Add invalid route message page

--- src/App.tsx
+++ src/App.tsx
@@ -18,6 +18,7 @@ import JournalPage from './pages/JournalPage/JournalPage';
 import JournalEntryPage from './pages/JournalEntryPage/JournalEntryPage';
 import HowMiWiWorksDashboard from './pages/HowMiWiWorks/HowMiWiWorksDashboard';
 import Divider from '@mui/material/Divider';
+import MessagePage from './components/MessagePage/MessagePage';
 
 export const endpoint: string = process.env.NODE_ENV === 'production' ? (process.env.REACT_APP_GQL_ENDPOINT_PRODUCTION as string) : (process.env.REACT_APP_GQL_ENDPOINT_DEVELOPMENT as string)
 
@@ -44,6 +45,7 @@ const App = () => {
       <Route path={`${PossibleRoutes.PROFILE_USER}`} element={<ProfilePage />} />
       <Route path={`${PossibleRoutes.JOURNAL_ENTRIES}`} element={<JournalPage />} />
       <Route path={`${PossibleRoutes.JOURNAL_ENTRIES_ENTRY}`} element={<JournalEntryPage />} />
+      <Route path="*" element={<MessagePage title="Uh oh, page not found." subtitle="Please enter a valid url or select from the the links below."/>} />
     </Routes>
   );
   
