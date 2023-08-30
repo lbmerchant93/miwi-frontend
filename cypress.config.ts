b5024379db12861a8859a6d6e979e8090490932b
Lucas Merchant <lbmerchant93@gmail.com>
2023-08-30 14:05:18 -0600

    Install dotenv, move service account to env file

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,14 +1,17 @@
 import { defineConfig } from "cypress";
-import * as serviceAccount from "./serviceAccount.json";
+// import * as serviceAccount from "./serviceAccount.json";
 const admin = require("firebase-admin");
 const cypressFirebasePlugin = require("cypress-firebase").plugin;
+require("dotenv").config();
 
+const serviceAccount = process.env.REACT_APP_SERVICE_ACCOUNT;
+if (!serviceAccount) throw new Error('The $GOOGLE_CREDS environment variable was not found!');
 
 export default defineConfig({
   e2e: {
     async setupNodeEvents(on, config) {
       cypressFirebasePlugin(on, config, admin, {
-        credential: admin.credential.cert(serviceAccount)
+        credential: admin.credential.cert(JSON.parse(serviceAccount))
       });
     },
     baseUrl: 'http://localhost:3000',
