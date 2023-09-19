8838175a4cc54b666b5452a5341e1d792ced629e
Lucas Merchant <lbmerchant93@gmail.com>
2023-09-19 11:47:53 -0600

    Ignore warning for now

--- src/App.authProvider.tsx
+++ src/App.authProvider.tsx
@@ -71,6 +71,7 @@ const AuthProvider: React.FC<AuthProviderProps> = (props) => {
         return () => {
             listen();
         }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
     }, []);
     
 
